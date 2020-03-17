6ad63ec8fd5120428ed99ccbcd650b3608c537a6
chad1994 <chad1994@skuniv.ac.kr>
2020-03-17 18:46:22 +0900

    openCv 4.2.0 버전 적용 & 사진 유사도 계산 로직 추가

--- app/src/main/java/com/anliban/team/hippho/di/module/ActivityBindingModule.kt
+++ app/src/main/java/com/anliban/team/hippho/di/module/ActivityBindingModule.kt
@@ -1,8 +1,8 @@
 package com.anliban.team.hippho.di.module
 
 import com.anliban.team.hippho.di.annotations.ActivityScoped
-import com.anliban.team.hippho.ui.MainActivity
-import com.anliban.team.hippho.ui.MainActivityModule
+import com.anliban.team.hippho.ui.main.MainActivity
+import com.anliban.team.hippho.ui.main.MainActivityModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
---
+++ app/src/main/java/com/anliban/team/hippho/ui/main/MainActivityModule.kt
@@ -0,0 +1,17 @@
+package com.anliban.team.hippho.ui.main
+
+import androidx.lifecycle.ViewModel
+import com.anliban.team.hippho.di.annotations.ViewModelKey
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+
+@Module
+abstract class MainActivityModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    internal abstract fun bindViewModel(viewModel: MainViewModel): ViewModel
+}
