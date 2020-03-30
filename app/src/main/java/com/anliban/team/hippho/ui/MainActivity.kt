e29458bde7cebe046b1fac3f9794831af59b086b
leehoyong <lee199402@gmail.com>
2020-03-30 20:56:01 +0900

    Configuration toolbar with navigation

--- app/src/main/java/com/anliban/team/hippho/ui/MainActivity.kt
+++ app/src/main/java/com/anliban/team/hippho/ui/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.anliban.team.hippho.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.view.doOnLayout
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.anliban.team.hippho.R
 import com.anliban.team.hippho.databinding.ActivityMainBinding
 import com.anliban.team.hippho.util.activityViewModel
@@ -23,26 +30,46 @@ class MainActivity : DaggerAppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private var navFragment: NavHostFragment? = null
+    private val navController: NavController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment).navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
         binding.apply {
             viewModel = this@MainActivity.viewModel
             lifecycleOwner = this@MainActivity
         }
 
-      /*  navFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navFragment?.navController*/
-
-       /* navFragment?.let {
-            NavigationUI.setupWithNavController(binding.bottomNavigation, it.navController)
+        binding.toolbar.doOnLayout {
+            invalidateOptionsMenu()
         }
 
-        navController?.addOnDestinationChangedListener { _, destination, _ ->
-            renderBottom(destination.id)
-        }*/
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            onDestinationChanged(destination)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+
+    private fun onDestinationChanged(destination: NavDestination) {
+        when (destination.id) {
+            R.id.mainFragment -> {
+                supportActionBar?.title = "Hippo"
+                binding.toolbar.navigationIcon = null
+            }
+            R.id.detailFragment -> {
+                supportActionBar?.title = "Detail"
+                binding.toolbar.navigationIcon = getDrawable(R.drawable.ic_arrow_back_black_24dp)
+            }
+        }
     }
 }
