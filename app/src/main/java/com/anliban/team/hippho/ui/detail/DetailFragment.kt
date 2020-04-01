3c86a4548c08b6421b949be6cf0d6a96787912f1
leehoyong <lee199402@gmail.com>
2020-04-02 01:16:07 +0900

    상세화면 메뉴 추가

--- app/src/main/java/com/anliban/team/hippho/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/anliban/team/hippho/ui/detail/DetailFragment.kt
@@ -2,10 +2,14 @@ package com.anliban.team.hippho.ui.detail
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.anliban.team.hippho.R
 import com.anliban.team.hippho.databinding.FragmentDetailBinding
 import com.anliban.team.hippho.util.viewModel
 import dagger.android.support.DaggerFragment
@@ -37,8 +41,23 @@ class DetailFragment : DaggerFragment() {
             viewModel = this@DetailFragment.viewModel
             lifecycleOwner = viewLifecycleOwner
         }
-
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.detail, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.check -> {
+
+                return false
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
