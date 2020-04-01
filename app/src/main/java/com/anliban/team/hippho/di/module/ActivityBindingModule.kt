f20c24d451b0f71814df88cf453c1d779d22f107
leehoyong <lee199402@gmail.com>
2020-04-01 23:34:22 +0900

    설정화면 추가

--- app/src/main/java/com/anliban/team/hippho/di/module/ActivityBindingModule.kt
+++ app/src/main/java/com/anliban/team/hippho/di/module/ActivityBindingModule.kt
@@ -6,6 +6,7 @@ import com.anliban.team.hippho.ui.MainActivityModule
 import com.anliban.team.hippho.ui.detail.DetailModule
 import com.anliban.team.hippho.ui.home.HomeModule
 import com.anliban.team.hippho.ui.info.InfoModule
+import com.anliban.team.hippho.ui.setting.SettingModule
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -19,7 +20,8 @@ abstract class ActivityBindingModule {
             MainActivityModule::class,
             DetailModule::class,
             HomeModule::class,
-            InfoModule::class
+            InfoModule::class,
+            SettingModule::class
         ]
     )
     internal abstract fun mainActivity(): MainActivity
