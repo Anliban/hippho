df47f06cc4b4ee7cfa320d1ad4c2635b6b8082c3
chad1994 <chad1994@skuniv.ac.kr>
2020-04-09 16:40:33 +0900

    useCase에 시간 간격 그룹핑처리 추가

--- app/src/main/java/com/anliban/team/hippho/domain/GetImageByDateUseCase.kt
+++ app/src/main/java/com/anliban/team/hippho/domain/GetImageByDateUseCase.kt
@@ -1,4 +1,5 @@
 package com.anliban.team.hippho.domain
+import com.anliban.team.hippho.data.ImageLoadHelper
 import com.anliban.team.hippho.data.ImageLoader
 import com.anliban.team.hippho.domain.model.GetImageRequestParameters
 import com.anliban.team.hippho.ui.home.HomeListContent
@@ -9,16 +10,19 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class GetImageByDateUseCase(
-    private val imageLoader: ImageLoader
+    private val imageLoader: ImageLoader,
+    private val imageLoadHelper: ImageLoadHelper
 ) : FlowUseCase<GetImageRequestParameters, List<HomeUiModel>>() {
 
     override fun execute(parameters: GetImageRequestParameters): Flow<List<HomeUiModel>> {
         return imageLoader.getImages(parameters.option)
             .map { images ->
                 images.groupBy { it.date.midNight() }
+                    .flatMap { imageLoadHelper.groupByTimeInterval(it.key, it.value) }
+                    .groupBy { it.first }
                     .flatMap {
                         val header = HomeListHeader(it.key)
-                        val content = HomeListContent(it.value)
+                        val content = (it.value.map { it2 -> HomeListContent(it2.second) })
                         listOf(header) + content
                     }
             }
