0be37412a662c349e95c8b2804d7856624e29f5e
chad1994 <chad1994@skuniv.ac.kr>
2020-04-14 17:54:08 +0900

    Double 소수점 2자리 반올림 & Byte->MegaByte Util함수 작성

--- app/src/main/java/com/anliban/team/hippho/util/StringUtil.kt
+++ app/src/main/java/com/anliban/team/hippho/util/StringUtil.kt
@@ -3,3 +3,7 @@ package com.anliban.team.hippho.util
 /**
  * Created by choejun-yeong on 14/04/2020.
  */
+
+fun bytesToMegaBytes(number: String): Double {
+    return number.toDouble() / 1024 / 1024
+}
