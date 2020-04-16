9b58b529a4dd24e9818fe612d54c398fb137a51d
leehoyong <lee199402@gmail.com>
2020-04-17 02:22:17 +0900

    갯수, 용량 strings format 추가 및 usecase에서 불러오는 로직 추가

--- app/src/main/java/com/anliban/team/hippho/util/StringUtil.kt
+++ app/src/main/java/com/anliban/team/hippho/util/StringUtil.kt
@@ -1,9 +1,13 @@
 package com.anliban.team.hippho.util
 
+import java.text.NumberFormat
+
 /**
  * Created by choejun-yeong on 14/04/2020.
  */
 
-fun bytesToMegaBytes(number: String): Double {
-    return number.toDouble() / 1024 / 1024
+fun Number.bytesToMegaBytes(): Double {
+    return toDouble() / 1024 / 1024
 }
+
+fun Number.toDecimal(): String = NumberFormat.getInstance().format(this)
